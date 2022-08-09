beb9b9fe0736452cee70c237970826d61a355543
sushanthc00 <sushanth15900@gmail.com>
2022-08-09 21:14:34 +0530

    [FINAL] Added currentAccount method

--- src/InterestCalculation.java
+++ src/InterestCalculation.java
@@ -16,10 +16,34 @@ public class InterestCalculation {
 
         System.out.println("Interest for the first quarter = " + interest);
         System.out.println("Amount in the account after first quarter = " + newBalanceSavings);
+        System.out.println();
+    }
+
+    public static void currentAccount(){
+        float initialBalance =50000;
+        float deposit = 10000;
+        float newBalance = initialBalance+ deposit;
+        float overdraft = 0.2f;
+        float withdrawAmount = 70000;
+        float maxWithdraw = 0;
+        float remainingBalance = 0;
+
+            maxWithdraw = newBalance + newBalance*overdraft;
+            remainingBalance = maxWithdraw - withdrawAmount;
+
+            if (maxWithdraw > withdrawAmount){
+                System.out.println("Yes, the requested funds can be withdrawn");
+                System.out.println("Your new balance after withdrawal is: " + remainingBalance);
+            }
+            else{
+                System.out.println("Insufficient Funds");
+            }
+
     }
 
     public static void main(String[] args){
         savingsAccount();
+        currentAccount();
 
     }
 }
