51e47635c134633697eb375122df2c2458b67783
sushanthc00 <sushanth15900@gmail.com>
2022-08-09 21:43:05 +0530

    [ADD] StudentsMarks class + inputs

---
+++ src/StudentsMarks.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StudentsMarks {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of Students ");
+        int numberOfStudents=sc.nextInt();
+        System.out.println("Enter the number of Subjects ");
+        int numberOfSubjects=sc.nextInt();
+
+        int[][] marksGrid = new int[numberOfStudents][numberOfSubjects];
+
+        for (int i=0;i<numberOfStudents;i++){
+            for(int j=0;j<numberOfSubjects;j++){
+
+
+            }
+        }
+
+
+
+    }
+}
